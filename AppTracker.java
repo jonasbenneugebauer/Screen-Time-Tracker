abe951557df74bdcccbd59272792d1a33c704698
Jonas Neugebauer <jonasneugebauer.01@gmx.de>
2026-03-27 17:30:20 +0100

    made first classes for testing

---
+++ AppTracker.java
@@ -0,0 +1,13 @@
+public class AppTracker {
+
+    private SessionManager sessionManager;
+    private TrackerGUI trackerGUI;
+
+    public AppTracker() {
+        sessionManager = new SessionManager();
+        trackerGUI = new TrackerGUI();
+    }
+
+
+
+}
