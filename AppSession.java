8d5608a89bceca25a2969f4be28b7e89e5e6718a
Jonas Neugebauer <jonasneugebauer.01@gmx.de>
2026-03-28 14:07:06 +0100

    added methods and constructor in AppSession

--- AppSession.java
+++ AppSession.java
@@ -1,3 +1,27 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class AppSession {
-    
+
+    private String appName;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    public AppSession(String appName, LocalDateTime startTime) {
+        this.appName = appName;
+        this.startTime = startTime;
+    }
+
+    public void end(){
+        this.endTime = LocalDateTime.now();
+    }
+
+    public long getDuration(){
+        if(endTime==null){
+            return 0;
+        } else {
+            return Duration.between(startTime, endTime).toSeconds();  
+        }
+    }
+
 }
